d47e7665b2cc69f296c35396224c218ea5917b70
amartid <64313984+amartid@users.noreply.github.com>
2023-06-30 18:02:28 +0300

    CheckBox programs 65

---
+++ src/CheckBox62/checkBox.java
@@ -0,0 +1,19 @@
+package CheckBox62;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+class checkBox {
+    public static void main(String[] args){
+        Gui go = new Gui();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,250);
+        go.setVisible(true);
+    }
+
+}
---
+++ src/CheckBox64/checkBox.java
@@ -0,0 +1,13 @@
+package CheckBox64;
+
+import javax.swing.*;
+
+class checkBox {
+    public static void main(String[] args){
+        Gui go = new Gui();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(250,100);
+        go.setVisible(true);
+    }
+
+}
